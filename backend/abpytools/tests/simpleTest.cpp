edb420621c207dc07e4ecc269560ec4c4ca6e55d
Gil <g.f.hoben@gmail.com>
2018-02-20 08:50:28 +0000

    first unit tests

--- backend/abpytools/tests/simpleTest.cpp
+++ backend/abpytools/tests/simpleTest.cpp
@@ -4,10 +4,16 @@
 
 #define BOOST_TEST_MODULE My Test 1
 #include <boost/test/included/unit_test.hpp>
+#include "chain.h"
+#include "exception.h"
 
 BOOST_AUTO_TEST_CASE(first_test)
 {
-    int i = 1;
-    BOOST_TEST(i);
-    BOOST_TEST(i == 2);
+    try {
+        auto *testObject = new AntibodyChainCPP;
+    }
+
+    catch (ModuleImportException &e) {
+        BOOST_FAIL("Failed to initialise AntibodyChainCPP object!");
+    }
 }
