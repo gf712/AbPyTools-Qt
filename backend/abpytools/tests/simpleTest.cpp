0b176fe8e3a62d1801d27aa1620cc84a1e1578e8
Gil <g.f.hoben@gmail.com>
2018-02-20 11:32:51 +0000

    test python object access

--- backend/abpytools/tests/simpleTest.cpp
+++ backend/abpytools/tests/simpleTest.cpp
@@ -2,12 +2,13 @@
 // Created by gil on 19/02/18.
 //
 
-#define BOOST_TEST_MODULE My Test 1
+#define BOOST_TEST_MODULE AbPyToolsPyCInterface
 #include <boost/test/included/unit_test.hpp>
 #include "chain.h"
 #include "exception.h"
+#include <string>
 
-BOOST_AUTO_TEST_CASE(first_test)
+BOOST_AUTO_TEST_CASE(ModuleImportTest)
 {
     try {
         auto *testObject = new AntibodyChainCPP;
@@ -16,4 +17,28 @@ BOOST_AUTO_TEST_CASE(first_test)
     catch (ModuleImportException &e) {
         BOOST_FAIL("Failed to initialise AntibodyChainCPP object!");
     }
+}
+
+BOOST_AUTO_TEST_CASE(ChainImportTest)
+{
+    try {
+        auto *testObject = new AntibodyChainCPP;
+    }
+
+    catch (ClassImportException &e) {
+        BOOST_FAIL("Failed to initialise AntibodyChainCPP object!");
+    }
+}
+
+BOOST_AUTO_TEST_CASE(NameTest)
+{
+
+    char name[5] = "test";
+    char sequence[5] = "ABCD";
+    char numbering_scheme[10] = "Chothia";
+
+    auto *testObject = new AntibodyChainCPP(sequence, name, numbering_scheme);
+
+    BOOST_TEST(testObject->getName() == name);
+
 }
