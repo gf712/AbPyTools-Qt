b64c9e42975f53d52343c475cca1d4684814ee26
Gil <g.f.hoben@gmail.com>
2018-02-26 17:12:24 +0000

    open sequence input dialog from mainwindow

--- gui/include/mainwindow.h
+++ gui/include/mainwindow.h
@@ -3,6 +3,11 @@
 
 #include "chain.h"
 #include <QMainWindow>
+#include <QTimer>
+#include <QString>
+#include <iostream>
+#include <QDebug>
+#include "newsequencedialog.h"
 
 namespace Ui {
 class MainWindow;
@@ -16,11 +21,27 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    // GETTERS
+    Ui::MainWindow* getUI() { return ui;}
+
 private slots:
     void on_actionOpen_triggered();
+    void on_actionNew_triggered();
+
+    void newSequenceDialogNameGetter() {
+
+    };
+
+    // DEBUG
+    void refresh_values();
 
 private:
     Ui::MainWindow *ui;
+    AntibodyChainCPP *antibodyObjects;
+    std::string name;
+    std::string sequence;
+    std::string numberingScheme;
+    QTimer *timer;
 };
 
 #endif // MAINWINDOW_H
--- gui/src/mainwindow.cpp
+++ gui/src/mainwindow.cpp
@@ -17,3 +17,27 @@ void MainWindow::on_actionOpen_triggered()
 {
 
 }
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    newSequenceDialogPointer = new newSequenceDialog(this);
+    newSequenceDialogPointer->show();
+
+//    name = newSequenceDialogPointer->getDialogName();
+//    sequence = newSequenceDialogPointer->getDialogSequence();
+//    numberingScheme = newSequenceDialogPointer->getDialogNumberingScheme();
+
+//    qDebug() << QString("name: %1\nsequence: %2\nnumbering scheme: %3").arg(QString::fromStdString(name),
+//                                                                            QString::fromStdString(sequence),
+//                                                                            QString::fromStdString(numberingScheme));
+
+}
+
+void MainWindow::refresh_values() {
+
+    ui->debugAreaTextEdit->setText(QString("name: %1\nsequence: %2\nnumbering scheme: %3").arg(QString::fromStdString(name),
+                                                                                               QString::fromStdString(sequence),
+                                                                                               QString::fromStdString(numberingScheme)));
+
+}
