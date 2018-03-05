8574ccfb993346e7a3d7789a58b8857aec08c30f
Gil <g.f.hoben@gmail.com>
2018-03-05 11:51:23 +0000

    sklearn style mlpack pca wrapper
    should implement more of these to make it easier to use..

---
+++ backend/abpytools-cpp/include/pca.h
@@ -0,0 +1,16 @@
+//
+// Created by gil on 05/03/18.
+//
+
+#ifndef ABPYTOOLS_QT_PCA_H
+#define ABPYTOOLS_QT_PCA_H
+
+
+
+class pca {
+
+};
+
+
+
+#endif //ABPYTOOLS_QT_PCA_H
---
+++ backend/abpytools-cpp/src/pca.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gil on 05/03/18.
+//
+
+#include "pca.h"
