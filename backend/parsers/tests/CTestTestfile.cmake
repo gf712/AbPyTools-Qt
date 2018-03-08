3b7e89fa520df655c03c02e69bd72606e860ed27
Gil <g.f.hoben@gmail.com>
2018-03-08 09:54:25 +0000

    renamed libraries and targets

--- backend/parsers/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gil/AbPyTools-Qt/backend/parsers/tests
-# Build directory: /home/gil/AbPyTools-Qt/backend/parsers/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
