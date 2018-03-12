2efdabfb1a338e9ac377e040c5a6f18312d4fc22
Gil <g.f.hoben@gmail.com>
2018-03-12 16:06:24 +0000

    fixed boost library names

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -16,7 +16,7 @@ then
     tar -xzf boost_1_66_0.tar.gz
     cd boost_1_66_0/
     sudo apt-get install autotools-dev libicu-dev build-essential libbz2-dev -y
-    ./bootstrap.sh --with-python-version=$TRAVIS_PYTHON_VERSION --with-libraries=unit_test_framework,python3,filesystem,system
+    ./bootstrap.sh --with-python-version=$TRAVIS_PYTHON_VERSION --with-libraries=test,python,filesystem,system
     ./b2 -j4
     sudo ./b2 install
     # qt
